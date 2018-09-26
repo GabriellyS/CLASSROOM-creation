a81ad26d3db491a0314386faf028f38218d3a747
Gabrielly <santos.gabrielly14@gmail.com>
2018-09-26 09:42:13 -0300

    commit teste

--- CriacaoTurmas/DAL/ProfessorDAO.cs
+++ CriacaoTurmas/DAL/ProfessorDAO.cs
@@ -38,5 +38,41 @@ namespace CriacaoTurmas.DAL
         {
             return ctx.Professores.FirstOrDefault(x => x.matricula.Equals(professor.matricula));
         }
+
+        public static bool AlterarProfessor(Professor professor)
+        {
+            try
+            {
+                var editado = ctx.Professores.FirstOrDefault(a => a.matricula == professor.matricula);
+                editado.nome = professor.nome;
+                editado.matricula = professor.matricula;
+
+                ctx.Entry(editado).State = System.Data.Entity.EntityState.Modified;
+                ctx.SaveChanges();
+                return true;
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static bool DeletarProfessor(Professor professor)
+        {
+
+            try
+            {
+                var deletado = ctx.Professores.FirstOrDefault(a => a.matricula == professor.matricula);
+                ctx.Professores.Remove(deletado);
+                ctx.SaveChanges();
+                return true;
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
     }
 }
